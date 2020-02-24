1222f00679e6ad2b606a8681d2478facae1b4ecf
Alex Smirnov <alks.smirnov@gmail.com>
2020-02-24 17:12:43 -0500

    Routes prep

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mogoURI');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+
+        console.log(`MongoDB Connected...`);
+    } catch(err) {
+        console.log(`Error connecting to DB: ${err.message}`);
+        // Exit the process with failure
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
