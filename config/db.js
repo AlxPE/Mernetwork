0122613077ff8cb4953e8cbe1d6fd236c33be3b5
AlexSmirnov <alks.smirnov@gmail.com>
2020-03-30 22:07:33 -0400

    Added Profile routes

--- config/db.js
+++ config/db.js
@@ -4,10 +4,15 @@ const db = config.get('mogoURI');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+        await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        });
 
         console.log(`MongoDB Connected...`);
-    } catch(err) {
+    } catch (err) {
         console.log(`Error connecting to DB: ${err.message}`);
         // Exit the process with failure
         process.exit(1);
