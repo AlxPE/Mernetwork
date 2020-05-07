d0ec8c4c3b3e865954a75edc43dfc2cad8353270
unknown <alks.smirnov@gmail.com>
2020-05-07 19:40:12 -0400

    Added Front Auth, Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navbar from './components/layout/Navbar';
@@ -10,10 +10,21 @@ import Login from './components/auth/Login';
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 
-const App = () => (
+if(localStorage.token) {
+    setAuthToken(localStorage.token);
+}
+
+const App = () => {
+    useEffect(() => {
+            store.dispatch(loadUser());
+    }, []);
+
+    return (
     <Provider store={store}>
         <Router>
             <Fragment>
@@ -29,7 +40,8 @@ const App = () => (
             </Fragment>
         </Router>
     </Provider>
-);
+)
+};
 
 
 //
